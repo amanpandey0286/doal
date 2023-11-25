21cf87ff3f20583d9cdef77e5bbf478c10427d3c
amanpandey0286 <amanpandey.2020@vitbhopal.ac.in>
2023-11-26 01:28:56 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
-import 'package:doal/pages/sign_in_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-import 'pages/sign_up_page.dart';
-import 'pages/todo_page.dart';
-import 'pallete.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
@@ -19,20 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.dark,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return HomePage();
-          } else {
-            return SignUpPage(email: '',);
-          }
-        },
+      home: Scaffold(
+        body: Center(
+          child: Text("Doal"),
+        ),
       ),
     );
   }
